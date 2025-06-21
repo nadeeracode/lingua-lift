158b9916864ee66a265639656ffbf677e69f8c60
Clythera Vox <nadeerawkz@gmail.com>
2025-06-21 12:59:59 +0530

    Started the server
    Server is ready and running with Express and Prisma

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/api/health', (req, res) => {
+  res.json({ message: 'LinguaLift API is running!' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;
