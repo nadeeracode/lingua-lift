83e56c368df1955924ab8bbdf7b3833e1021e8f1
Clythera Vox <nadeerawkz@gmail.com>
2025-06-21 12:23:47 +0530

    Initial Client Files
    Initial files of the client app.

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">React App
+      </header>
+    </div>
+  );
+}
+
+export default App;
